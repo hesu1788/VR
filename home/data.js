ad666ee4f85948857eba946adbc71fb23a5b42a3
hesu1788 <sunnyhsu0109@gmail.com>
2025-04-25 11:04:15 +0800

    vr

---
+++ home/data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "貓空站",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.2492692457925365,
+        "pitch": -0.03234353540522861,
+        "fov": 0.6818652311382287
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -1.0786431357651978,
+          "pitch": -0.17989851570222015,
+          "title": "貓空站",
+          "text": "貓空纜車終點站，內部有遊客中心、便利商店。"
+        }
+      ]
+    }
+  ],
+  "name": "HOME",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
